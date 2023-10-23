9d8b04bf51ea2d3a20748e03c87aa6d015b1e3da
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-10-23 11:30:44 +0300

    adding the app and index js files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./app.css";
+import Main from "./components/main/main";
+import Nav from "./components/nav/nav";
+function App() {
+	return (
+		<div className="app">
+			<Nav />
+			<Main />
+		</div>
+	);
+}
+
+export default App;
